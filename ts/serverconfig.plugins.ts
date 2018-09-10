4073a7af42db5f9180e8b14409261823e00fff12
Phil Kunz <gitlab@philkunz.com>
2018-09-10 18:07:38 +0200

    add basic configuration options

--- ts/serverconfig.plugins.ts
+++ ts/serverconfig.plugins.ts
@@ -1,4 +1,7 @@
+import * as consolecolor from '@pushrocks/consolecolor';
 import * as smartcli from '@pushrocks/smartcli';
 import * as smartfile from '@pushrocks/smartfile';
+import * as smartinteract from '@pushrocks/smartinteract';
+import * as smartlog from '@pushrocks/smartlog';
 
-export { smartcli, smartfile };
+export { consolecolor, smartcli, smartfile, smartinteract, smartlog };
