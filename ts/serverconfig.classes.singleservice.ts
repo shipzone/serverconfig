af638f7d27d9eb466718db162800d6a177f9f516
Phil Kunz <github@philkunz.com>
2018-09-14 01:02:32 +0200

    fix(core): update

--- ts/serverconfig.classes.singleservice.ts
+++ ts/serverconfig.classes.singleservice.ts
@@ -25,7 +25,7 @@ export class SingleService {
     await smartshellInstance.exec('docker network create -d overlay --attachable webgateway');
 
     // lets install the traefik stack
-    await smartshellInstance.exec(`cd ${paths.singleServiceDir} && chmod 600 ./configs/acme.json )`);
+    await smartshellInstance.exec(`(cd ${paths.singleServiceDir} && chmod 600 ${paths.singleServiceDir}/configs/acme.json)`);
     await smartshellInstance.exec(`(cd ${paths.singleServiceDir} && docker stack deploy --compose-file docker-compose.yml traefikstack)`);
   }
 }
